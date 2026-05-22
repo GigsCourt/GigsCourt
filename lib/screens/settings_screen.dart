aca4f00527e2cdd8cd917dd587b0e53c22dc8df8
GigsCourt <gigscourt@gmail.com>
2026-05-22 09:29:53 +0100

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -218,7 +218,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                       ),
                     ),
                   );
-                }),
+                }).toList(),
             ],
           ),
         ),
