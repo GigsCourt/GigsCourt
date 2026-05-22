dabbdedbec7d4236909b8c65015dfcc1baa5db0a
GigsCourt <gigscourt@gmail.com>
2026-05-22 11:24:33 +0100

    Create app_config.dart

---
+++ lib/config/app_config.dart
@@ -0,0 +1,4 @@
+class AppConfig {
+  static const String paystackPublicKey = String.fromEnvironment('PAYSTACK_PUBLIC_KEY');
+  static const String paystackSecretKey = String.fromEnvironment('PAYSTACK_SECRET_KEY');
+}
